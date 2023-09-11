3e8a39e01e147c49455a6ea5089009aea0e23ed1
Joseph Harry <findarato@gmail.com>
2023-09-11 08:46:08 -0500

    :rewind: chore: Reverting swag config to upstream Signed-off-by: Joseph Harry <findarato@gmail.com>

---
+++ roles/docker-common/files/cleanup_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if hash podman 2>/dev/null; then
+    # Delete all containers
+    podman rm $(podman ps -a -q)
+    # Delete all images
+    podman rmi $(podman images -q)
+elif hash docker 2>/dev/null; then
+    # Delete all containers
+    docker rm $(docker ps -a -q)
+    # Delete all images
+    docker rmi $(docker images -q)
+fi
